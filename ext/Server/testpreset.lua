9206e82f0838f7df1a2066505c6d4c70d3568691
Mats Bakken <powback@gmail.com>
2020-12-21 13:40:31 +0100

    Inital commit

---
+++ ext/Server/testpreset.lua
@@ -0,0 +1,265 @@
+return [[
+{
+  "header": {
+    "projectName": "Test",
+    "mapName": "XP2_Skybar",
+    "gamemodeName": "TeamDeathMatch0",
+    "requiredBundles": {
+      "Levels/XP2_Skybar/XP2_Skybar": true,
+      "Levels/MP_Subway/MP_Subway_Settings_win32": true,
+      "gameconfigurations/game": true,
+      "Levels/XP2_Skybar/TeamDM": true,
+      "Levels/XP2_Skybar/DeathMatch": true
+    }
+  },
+  "data": {
+    "3c184c55-13a7-95d4-b20a-bcd76c72b39c": {
+      "blueprintCtrRef": {
+        "partitionGuid": "B988B56C-3C42-11E1-8B5D-DC35B0EA2BA5",
+        "instanceGuid": "734F4398-39E1-A1FF-1632-A93505FB16E2",
+        "typeName": "ObjectBlueprint",
+        "name": "XP2/Objects/Pergola_01/Pergola_01"
+      },
+      "variation": 0,
+      "name": "XP2/Objects/Pergola_01/Pergola_01",
+      "originalRef": {},
+      "isVanilla": false,
+      "guid": "3c184c55-13a7-95d4-b20a-bcd76c72b39c",
+      "transform": {
+        "left": {
+          "x": 1,
+          "y": 0,
+          "z": 0
+        },
+        "up": {
+          "x": 0,
+          "y": 0,
+          "z": 1
+        },
+        "forward": {
+          "x": 0,
+          "y": -1,
+          "z": 0
+        },
+        "trans": {
+          "x": 32.988887786865,
+          "y": 13,
+          "z": 8
+        }
+      },
+      "parentData": {
+        "guid": "00000000-0000-0000-0000-000000000000",
+        "partitionGuid": "00000000-0000-0000-0000-000000000000",
+        "typeName": "custom_root",
+        "primaryInstanceGuid": "00000000-0000-0000-0000-000000000000"
+      }
+    },
+    "d67f4604-e6dc-3d81-3687-ccf09c01fd2e": {
+      "blueprintCtrRef": {
+        "partitionGuid": "B988B56C-3C42-11E1-8B5D-DC35B0EA2BA5",
+        "instanceGuid": "734F4398-39E1-A1FF-1632-A93505FB16E2",
+        "typeName": "ObjectBlueprint",
+        "name": "XP2/Objects/Pergola_01/Pergola_01"
+      },
+      "variation": 0,
+      "name": "XP2/Objects/Pergola_01/Pergola_01",
+      "originalRef": {},
+      "overrides": {},
+      "isVanilla": false,
+      "guid": "d67f4604-e6dc-3d81-3687-ccf09c01fd2e",
+      "transform": {
+        "left": {
+          "x": 1,
+          "y": 0,
+          "z": 0
+        },
+        "up": {
+          "x": 0,
+          "y": 0,
+          "z": -1
+        },
+        "forward": {
+          "x": 0,
+          "y": 1,
+          "z": 0
+        },
+        "trans": {
+          "x": 32.988887786865,
+          "y": 13,
+          "z": 8
+        }
+      },
+      "parentData": {
+        "guid": "00000000-0000-0000-0000-000000000000",
+        "partitionGuid": "00000000-0000-0000-0000-000000000000",
+        "typeName": "custom_root",
+        "primaryInstanceGuid": "00000000-0000-0000-0000-000000000000"
+      }
+    },
+    "680b7784-be55-6289-5bb5-ee5946647352": {
+      "blueprintCtrRef": {
+        "partitionGuid": "CA2672FB-561E-11E1-9E4B-FE757C1FE0EB",
+        "instanceGuid": "010C8D55-2E24-E039-2264-6F3E7D38233D",
+        "typeName": "ObjectBlueprint",
+        "name": "XP2/Objects/Plant_01/Plant_01"
+      },
+      "variation": 0,
+      "name": "XP2/Objects/Plant_01/Plant_01",
+      "originalRef": {},
+      "isVanilla": false,
+      "guid": "680b7784-be55-6289-5bb5-ee5946647352",
+      "transform": {
+        "left": {
+          "x": 1,
+          "y": 0,
+          "z": 0
+        },
+        "up": {
+          "x": 0,
+          "y": 1,
+          "z": 0
+        },
+        "forward": {
+          "x": 0,
+          "y": 0,
+          "z": 1
+        },
+        "trans": {
+          "x": 30.666910171509,
+          "y": 10.881366729736,
+          "z": 9.9403028488159
+        }
+      },
+      "parentData": {
+        "guid": "00000000-0000-0000-0000-000000000000",
+        "partitionGuid": "00000000-0000-0000-0000-000000000000",
+        "typeName": "custom_root",
+        "primaryInstanceGuid": "00000000-0000-0000-0000-000000000000"
+      }
+    },
+    "0954ca7c-0de3-dd69-7bf8-776edef6bbbc": {
+      "blueprintCtrRef": {
+        "partitionGuid": "CA2672FB-561E-11E1-9E4B-FE757C1FE0EB",
+        "instanceGuid": "010C8D55-2E24-E039-2264-6F3E7D38233D",
+        "typeName": "ObjectBlueprint",
+        "name": "XP2/Objects/Plant_01/Plant_01"
+      },
+      "variation": 0,
+      "name": "XP2/Objects/Plant_01/Plant_01",
+      "originalRef": {},
+      "isVanilla": false,
+      "guid": "0954ca7c-0de3-dd69-7bf8-776edef6bbbc",
+      "transform": {
+        "left": {
+          "x": 1,
+          "y": 0,
+          "z": 0
+        },
+        "up": {
+          "x": 0,
+          "y": 1,
+          "z": 0
+        },
+        "forward": {
+          "x": 0,
+          "y": 0,
+          "z": 1
+        },
+        "trans": {
+          "x": 33.24390411377,
+          "y": 10.881367683411,
+          "z": 5.5803427696228
+        }
+      },
+      "parentData": {
+        "guid": "00000000-0000-0000-0000-000000000000",
+        "partitionGuid": "00000000-0000-0000-0000-000000000000",
+        "typeName": "custom_root",
+        "primaryInstanceGuid": "00000000-0000-0000-0000-000000000000"
+      }
+    },
+    "8b90918f-49f3-424f-f62b-bb6a4cb60550": {
+      "blueprintCtrRef": {
+        "partitionGuid": "CA2672FB-561E-11E1-9E4B-FE757C1FE0EB",
+        "instanceGuid": "010C8D55-2E24-E039-2264-6F3E7D38233D",
+        "typeName": "ObjectBlueprint",
+        "name": "XP2/Objects/Plant_01/Plant_01"
+      },
+      "variation": 0,
+      "name": "XP2/Objects/Plant_01/Plant_01",
+      "originalRef": {},
+      "isVanilla": false,
+      "guid": "8b90918f-49f3-424f-f62b-bb6a4cb60550",
+      "transform": {
+        "left": {
+          "x": 1,
+          "y": 0,
+          "z": 0
+        },
+        "up": {
+          "x": 0,
+          "y": 1,
+          "z": 0
+        },
+        "forward": {
+          "x": 0,
+          "y": 0,
+          "z": 1
+        },
+        "trans": {
+          "x": 32.512790679932,
+          "y": 11.929428100586,
+          "z": 0.72303128242493
+        }
+      },
+      "parentData": {
+        "guid": "00000000-0000-0000-0000-000000000000",
+        "partitionGuid": "00000000-0000-0000-0000-000000000000",
+        "typeName": "custom_root",
+        "primaryInstanceGuid": "00000000-0000-0000-0000-000000000000"
+      }
+    },
+    "ED170122-0000-0000-0000-011738439784": {
+      "blueprintCtrRef": {
+        "partitionGuid": "FE965069-835B-4930-885B-F8BA666129E6",
+        "instanceGuid": "EB036757-BA12-49F8-9A3A-A3BFCAB00DCA",
+        "typeName": "WorldPartData",
+        "name": "Levels/XP2_Skybar/Floor_1_Left"
+      },
+      "variation": 0,
+      "name": "Levels/XP2_Skybar/Floor_1_Left",
+      "originalRef": {
+        "partitionGuid": "2DF41167-0BAB-11E1-AA4E-EFBA5D767A10",
+        "instanceGuid": "7036CDA2-ADC4-4AF8-AA17-EC49F02D115F",
+        "typeName": "WorldPartReferenceObjectData"
+      },
+      "overrides": {},
+      "isVanilla": true,
+      "guid": "ED170122-0000-0000-0000-011738439784",
+      "transform": {
+        "left": {
+          "x": 1,
+          "y": 0,
+          "z": 0
+        },
+        "up": {
+          "x": 0,
+          "y": 1,
+          "z": 0
+        },
+        "forward": {
+          "x": 0,
+          "y": 0,
+          "z": 1
+        },
+        "trans": {
+          "x": 73.004821777344,
+          "y": 0,
+          "z": 6.5945072174072
+        }
+      },
+      "parentData": {}
+    }
+  }
+}
+]]
